2fceccec97f2e84754968878806450cc64756043
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-10-13 13:03:36 +0200

    Initial import

---
+++ AvaloniaMinimized/App.axaml.cs
@@ -0,0 +1,63 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaMinimized
+{
+    public class App : Application
+    {
+        public App()
+        {
+            DataContext = this;
+        }
+        
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            // if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            // {
+            //     desktop.MainWindow = new MainWindow();
+            // }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+
+        private int count = 0;
+        
+        public void ToggleShow()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                count++;
+                if (count == 2)
+                {
+                    count = 0;
+                    return;
+                }
+                if (desktop.MainWindow is null)
+                {
+                    desktop.MainWindow = new MainWindow();
+                    desktop.MainWindow.WindowState = WindowState.Normal;
+                    desktop.MainWindow.Show();
+                    return;
+                }
+
+                if (desktop.MainWindow.WindowState == WindowState.Minimized)
+                {
+                    desktop.MainWindow.WindowState = WindowState.Normal;
+                    desktop.MainWindow.Show();
+                }
+                else
+                {
+                    desktop.MainWindow.WindowState = WindowState.Minimized;
+                    desktop.MainWindow.Hide();
+                }
+            }
+        }
+    }
+}
